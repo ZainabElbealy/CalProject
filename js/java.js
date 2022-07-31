54814ad03a4be9537efd6f0aa230f9547c8dffd2
zainab <zainab96ahmed@gmail.com>
2022-07-31 04:49:35 +0200

    Projects

---
+++ js/java.js
@@ -0,0 +1,58 @@
+let myBtns , myEqual , myClear ,myResult;
+
+myBtns = document.querySelectorAll('.btns .btn')
+myEqual= document.querySelector('.equal')
+myClear= document.querySelector('.clear')
+myResult=document.querySelector('.result')
+myTime=document.querySelector('.Time h4')
+
+
+// console.log(myBtns)
+// console.log(myEqual)
+
+myBtns.forEach((show)=>{
+    show.addEventListener('click',(att)=>{
+        myResult.value+=att.target.dataset.name
+    })
+})
+
+myEqual.addEventListener('click',()=>{
+    // console.log(eval())
+    myResult.value=eval(myResult.value)
+})
+
+myClear.addEventListener('click',()=>{
+    myResult.value=''
+})
+
+
+// function showTime(){
+//     let myDate = new Date();
+//     let mySeconds = myDate.getSeconds()
+
+//     if (myDate.getSeconds()>=0 && myDate.getSeconds()<10){
+//         mySeconds ='0' +myDate.getSeconds()
+    
+//     }
+
+//     myTime.innerHTML=myDate.getHours() + ' : ' + myDate.getMinutes() + ' : '  +myDate.getSeconds()
+
+
+// }
+
+
+// setInterval(showTime, 1000);
+
+setInterval(() => {
+    let myDate = new Date();
+        let mySeconds = myDate.getSeconds()
+    
+        if (myDate.getSeconds()>=0 && myDate.getSeconds()<10){
+            mySeconds ='0' +myDate.getSeconds()
+        
+        }
+    
+        myTime.innerHTML=myDate.getHours() + ' : ' + myDate.getMinutes() + ' : '  +myDate.getSeconds()
+    
+    
+}, 1000);
